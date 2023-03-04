07c26cead85bb64fabeafe82b7c59b60f0a5d570
mizdra <pp.mizdra@gmail.com>
2023-03-04 19:07:30 +0900

    useMeasure 使ってサイズ算出

--- src/client/components/feature/ProductListSlider/hooks/useSlider.ts
+++ src/client/components/feature/ProductListSlider/hooks/useSlider.ts
@@ -1,11 +1,12 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useMeasure } from 'react-use';
 import { throttle } from 'throttle-debounce';
 
 const ITEM_MIN_WIDTH = 250 as const;
 
 export const useSlider = ({ items }: { items: unknown[] }) => {
-  console.log(items);
-  const containerElementRef = useRef<HTMLUListElement>(null);
+  const [containerElementRef, { width }] = useMeasure();
+
   const [visibleItemCount, setVisibleItemCount] = useState(1);
   const [_slideIndex, setSlideIndex] = useState(0);
   const slideIndex = Math.min(Math.max(0, _slideIndex), items.length - 1);
@@ -13,23 +14,11 @@ export const useSlider = ({ items }: { items: unknown[] }) => {
   useEffect(() => {
     const updateVisibleItemCount = throttle(500, () => {
       setVisibleItemCount(() => {
-        const containerWidth = containerElementRef.current?.getBoundingClientRect().width ?? 0;
-        return Math.max(Math.floor(containerWidth / ITEM_MIN_WIDTH), 1);
+        return Math.max(Math.floor(width / ITEM_MIN_WIDTH), 1);
       });
     });
-
-    let timer = (function tick() {
-      // TODO: もうちょっといい方法があるはず
-      return setTimeout(() => {
-        updateVisibleItemCount();
-        timer = tick();
-      }, 1000);
-    })();
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
+    updateVisibleItemCount();
+  }, [width]);
 
   return {
     containerElementRef,
