97f39a8dc62e55d2a9eddd2e6ee24eaa97d762db
mizdra <pp.mizdra@gmail.com>
2023-03-04 18:33:38 +0900

    AspectRatio 壊れてたので修正

--- src/client/components/foundation/AspectRatio/AspectRatio.styles.ts
+++ src/client/components/foundation/AspectRatio/AspectRatio.styles.ts
@@ -1,5 +1,7 @@
 import { css } from '@emotion/css';
 
 export const container = ({ ratioHeight, ratioWidth }: { ratioWidth: number; ratioHeight: number }) => css`
+  position: relative;
+  width: 100%;
   aspect-ratio: ${ratioWidth} / ${ratioHeight};
 `;
